9362bc0801da4502ce68179d98c524257f17be79
Matt Marlow <thequantumblink@gmail.com>
2025-02-08 13:26:08 -0600

    feat: added lab header checking

--- MUCSMake/mucsmake.py
+++ MUCSMake/mucsmake.py
@@ -5,6 +5,7 @@ import getpass
 import sys
 import toml
 import os 
+import re
 from datetime import datetime
 
 import tomlkit
@@ -23,20 +24,39 @@ date_format = "%Y-%m-%d_%H:%M:%S"
 
 
 def main(username, class_code, lab_name, file_name):
+    # Stage 1 - Prepare Configuration
     if not os.path.exists(CONFIG_FILE):
         print(f"{CONFIG_FILE} does not exist, creating a default one")
         prepare_toml_doc()
         print("You'll want to edit this with your correct information. Cancelling further program execution!")
         exit()
     config_obj = prepare_config_obj()
-    if not verify_lab_window(config_obj, lab_name):
-        print("Lab name not found")
-    if not retrieve_lab_window(config_obj, lab_name):
-        print("Bad")
-    
+    # Stage 2 - Prepare Compilation
+    lab_window_file_status = verify_lab_window(config_obj, lab_name)
+    if not lab_window_file_status:
+        print("*** Lab number missing or invalid. Please check again. ***")
+    lab_file_status = verify_lab_file_existence(config_obj, file_name)
+    if not lab_file_status:
+        print(f"*** Error: file {file_name} does not exist in the current directory. ***")
+    lab_header_inclusion = verify_lab_header_inclusion(config_obj, file_name, lab_name)
+    if not lab_header_inclusion:
+        print(f"*** Warning: your submission {file_name} does not include the lab header file. ***")
 
 
-
+# Uses a Regex string to detect if the lab header has been included in the file
+def verify_lab_header_inclusion(config_obj, file_name, lab_name):
+    search_pattern = f"^(#include)\s*(\"{lab_name}.h\")"
+    print(search_pattern)
+    with open(file_name, 'r') as c_file:
+        for line in c_file:
+            print(line)
+            if re.search(search_pattern, line):
+                return True
+    return False
+def verify_lab_file_existence(config_obj, file_name):
+    if os.path.exists(file_name):
+        return True
+    return False
 def retrieve_lab_window(config_obj, lab_name):
     with open(config_obj.lab_window_path, 'r', newline="") as window_list:
         next(window_list)
@@ -73,9 +93,15 @@ def prepare_toml_doc():
     doc = document()
 
     general = table()
+    general.add("class_code", "2050")
+    general.add(comment("Checks for a C header file corresponding to the lab name in the submission."))
+    general.add("check_lab_header", True)
+    
     paths = table()
+    paths.add("base_path", "/cluster/pixstor/class/")
     paths.add("lab_window_path", "")
-
+    paths.add("lab_submission_directory", "/submissions")
+    doc['general'] = general
     doc['paths'] = paths
 
 
@@ -98,6 +124,7 @@ def prepare_config_obj():
 
 
 if __name__ == "__main__":
+    # Stage 0 - Collect Command Args
     username = getpass.getuser()
     class_code = sys.argv[1]
     lab_name = sys.argv[2]
